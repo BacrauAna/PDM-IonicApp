7933ad7a67023d9c8147043fe0cd26a356233456
BacrauAna <anisoarabacrau1@gmail.com>
2020-11-04 16:42:18 +0200

    https://github.com/BacrauAna/PDM-IonicApp.git

---
+++ src/todo/itemApi.tsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { getLogger } from '../core';
+import { ItemProps } from './ItemProps';
+
+const log = getLogger('itemApi');
+
+const baseUrl = 'http://localhost:3000';
+const itemUrl = `${baseUrl}/item`;
+
+interface ResponseProps<T> {
+    data: T;
+}
+
+function withLogs<T>(promise: Promise<ResponseProps<T>>, fnName: string): Promise<T> {
+    log(`${fnName} - started`);
+    return promise
+        .then(res => {
+            log(`${fnName} - succeeded`);
+            return Promise.resolve(res.data);
+        })
+        .catch(err => {
+            log(`${fnName} - failed`);
+            return Promise.reject(err);
+        });
+}
+
+const config = {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
+export const getItems: () => Promise<ItemProps[]> = () => {
+    return withLogs(axios.get(itemUrl, config), 'getItems');
+}
+
+export const createItem: (item: ItemProps) => Promise<ItemProps[]> = item => {
+    return withLogs(axios.post(itemUrl, item, config), 'createItem');
+}
+
+export const updateItem: (item: ItemProps) => Promise<ItemProps[]> = item => {
+    return withLogs(axios.put(`${itemUrl}/${item.id}`, item, config), 'updateItem');
+}
